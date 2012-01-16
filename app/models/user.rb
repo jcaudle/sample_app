f4088a3f189124dba61f4749b2ab6b9f34695cde
Joseph Caudle <joseph.caudle@gmail.com>
2012-01-15 18:47:17 -0600

    Added migration for salt and encrypted passwords to users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,4 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
-
+require 'digest'
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
@@ -27,19 +17,40 @@ class User < ActiveRecord::Base
 
   before_save :encrypt_password
 
-  # Return true if the user's password matches the submitted password.
   def has_password?(submitted_password)
-    # Compare encrypted_password with the encrypted version of
-    # submitted_password
+    encrypted_password == encrypt(submitted_password)
   end
   
   private
 
     def encrypt_password
+      self.salt = make_salt unless has_password?(password)
       self.encrypted_password = encrypt(password)
     end
 
     def encrypt(string)
-      string # Only temporary!!!!
+      secure_hash("#{salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
     end
 end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
+#
+
