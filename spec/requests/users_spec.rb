11d5dc154f9fbd3f8a6d8de3c0d2d85938da9a14
Joseph Caudle <joseph.caudle@gmail.com>
2012-01-16 23:42:45 -0600

    Added and incorporated integration_sign_in helper.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -52,11 +52,8 @@ describe "Users" do
 
     describe "success" do
       it "should sign a user in and out" do
-        user = Factory(:user)
-        visit signin_path
-        fill_in :email,    :with => user.email
-        fill_in :password, :with => user.password
-        click_button
+        @user = Factory(:user)
+        integration_sign_in(@user)
         controller.should be_signed_in
         click_link "Sign out"
         controller.should_not be_signed_in
