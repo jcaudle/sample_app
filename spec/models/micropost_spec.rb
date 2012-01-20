f72d5e86994019146eaef1a53cdc14c3e34e13e4
Joseph Caudle <joseph.caudle@gmail.com>
2012-01-19 20:39:45 -0600

    Added validations on microposts.

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -7,8 +7,8 @@ describe Micropost do
     @attr = { :content => "value for content" }
   end
 
-  it "should create a new instance" do
-    Micropost.create!(@attr)
+  it "should create a new instance given valid attributes" do
+    @user.microposts.create!(@attr)
   end
 
   describe "user associations" do
@@ -24,6 +24,21 @@ describe Micropost do
     it "should have the right associated user" do
       @micropost.user_id.should == @user.id
       @micropost.user.should == @user
+    end
+  end
+
+  describe "validations" do
+
+    it "should require a user id" do
+      Micropost.new(@attr).should_not be_valid
+    end
+
+    it "should require nonblank content" do
+      @user.microposts.build(:content => " ").should_not be_valid
+    end
+
+    it "should reject long content" do
+      @user.microposts.build(:content => "a" * 141).should_not be_valid
     end
   end
 end
