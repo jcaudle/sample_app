bb6f216a9a690db727aaa6ccd323ad15486dc155
Joseph Caudle <joseph.caudle@gmail.com>
2012-01-20 00:56:23 -0600

    Added microposts to the user show page. Fixed a typo in custom.css.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,6 @@ class UsersController < ApplicationController
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user,   :only => :destroy
   
-  def show
-    @user = User.find(params[:id])
-    @title = @user.name
-  end
-  
   def new
     @user = User.new
     @title = "Sign up"
@@ -49,6 +44,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(:page => params[:page])
     @title = @user.name
   end
 
